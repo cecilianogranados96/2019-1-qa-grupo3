a5740d86e725b46375aee9aa09aa6f1bd6af4cbe
GalahadCr <isaacriverac94@gmail.com>
2019-04-02 11:52:34 -0600

    Update index.php

--- index.php
+++ index.php
@@ -10,83 +10,48 @@ if(isset($_SESSION['usuario']))
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="UTF-8">
-		<title>Añadir Usuario</title>
-        <link rel="stylesheet" type="text/css" href="css/style.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
-        <link href="https://fonts.googleapis.com/css?family=Coiny" rel="stylesheet">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+		<meta charset="utf-8">
+		  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+		  <title>Iniciar seccion </title>
+		  <!-- Tell the browser to be responsive to screen width -->
+		  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+		  <!-- Bootstrap 3.3.6 -->
+		  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+		  <!-- Font Awesome -->
+		  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+		  <!-- Ionicons -->
+		  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+		  <!-- Theme style -->
+		  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+		  <!-- iCheck -->
+		  <link rel="stylesheet" href="plugins/iCheck/square/blue.css">
+
 	</head>
-	<body>
-		<div class="row pt-5">
-		</div>
-		<div class="row">
-			<div class="col-2"></div>
-			<div class="col-8">
-			<form action="" method="post">
-				<div class="form-group row">
-					<!-- <label for="flname" class="col-sm-12 col-form-label labels">Usuario</label> -->
-					<input type="text" name="usuario" class="form-control" placeholder="Usuario">
-				</div>
+	<body class="hold-transition login-page">
+		<div class="login-box">
+  <div class="login-logo">
+    <a href=""><b>Compu</b>Mundo <b>Hiper</b>Mega<b>Red</b></a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+    <p class="login-box-msg">Bienvenido</p>
 
-				<div class="form-group row">
-					<!-- <label for="flname" class="col-sm-12 col-form-label labels">Usuario</label> -->
-					<input type="password" name="contrasenna" class="form-control" placeholder="Contraseña...">
-				</div>
-				<div class="form-group row">
-					<!-- <label for="flname" class="col-sm-12 col-form-label labels">Usuario</label> -->
-					<input type="submit" name="boton" value="Ingresar" class="form-control">
-				</div>
-			</form>
-			<?php
-				if(isset($_POST['boton']))
-				{
-					// echo "LISTO";
-					// session_start();
-					include("php/dataBase.php");
-					$usuario = $_POST['usuario'];
-					$contrasenna = $_POST['contrasenna'];
-					// echo $usuario . " - " . $contrasenna;
-					$objeto = new dataBase();
-					$result = $objeto->consultar("SELECT * FROM usuario");
-					$todo = "";
-					$objeto = 0;
-					$flag = True;
-					while (($fila = mysqli_fetch_array($result)) and $flag )
-					{
-						$todo = $todo . ", " . $fila['nombreUsuario'];
-						if ($fila['direccionUsuario'] == $usuario)
-						{
-							$objeto = $fila;
-							$flag = False;
-						}
-					}
-					
-					if ($flag)
-					{
-						echo "Usuario Incorrecto";
-					}
-					else
-					{
-						if ($objeto['contrasennaUsuario'] == $contrasenna)
-						{
-							$_SESSION['usuario'] = $objeto['nombreUsuario'];
-							$_SESSION['id'] = $objeto['idUsuario'];
-							// echo $objeto['nombreUsuario'] . "," . $objeto['apellidoUsuario'] . "," .  $objeto['direccionUsuario'] . "," .  $objeto['contrasennaUsuario'];
-							header('Location: inicio/');
-						}
-						else
-						{
-							echo "Contraseña incorrecta";
-						}
-					}
-				}
-			?>
-			</div>
-			<div class="col-2"></div>
-
-		</div>
-		
-    	<script src="js/script.js"/>
+    <form action="" method="post">
+      <div class="form-group has-feedback">
+        <input type="text" class="form-control" placeholder="Usuario" name="usuario" pattern="[A-Za-z_-0-9]{1,20}">
+        <span class="glyphicon glyphicon-user form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" class="form-control" placeholder="Contraseña" name="contrasena" pattern="[A-Za-z_-0-9]{1,20}">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <!-- /.col -->
+        <div class="col-xs-12">
+          <button type="submit" name="login" class="btn btn-primary btn-block btn-flat">Iniciar Sesión</button>
+        </div>
+        <!-- /.col -->
+      </div>
+    </form>
 	</body>
 </html>
