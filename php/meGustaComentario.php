3bd873862a17716b39343b2669dbe465d676b825
GalahadCr <isaacriverac94@gmail.com>
2019-04-26 20:39:06 -0600

    Update meGustaComentario.php

--- php/meGustaComentario.php
+++ php/meGustaComentario.php
@@ -1,21 +1,42 @@
 <?php
-	session_start();
-	// include 'lib/config.php';
+use PHPUnit\Framework\TestCase;
+include_once("dataBase.php");
 
-	if(!isset($_SESSION['usuario']))
-	{
-	  header("Location: ../");
-	}
-	else
-	{
-		include("dataBase.php");
-		$usuario = $_SESSION['id'];
-		$idPub = $_SESSION['idPublicacion'];
-		unset($_SESSION['idPublicacion']);
-		$comentario = $_GET['idComentario'];
-		$script = "insert into likeComentario (idComentario,idUsuarioLike) values (" . (int)$comentario . "," . (int)$usuario . ");";
-		$objeto = new dataBase();
-		$result = $objeto->insertar($script);
-		header("Location: ../inicio/verComentarios.php?idPublicacion=" . $idPub);
-	}
-?>
+    class TodoTest extends TestCase
+    {
+
+
+     public function meGustaComentario($usuario,$publicacion,$idComentario)
+        {     
+           
+    
+                $script = "insert into likeComentario (idComentario,idUsuarioLike) values (" . (int)$comentario . "," . (int)$usuario . ");";
+                $objeto = new dataBase();
+                $result = $objeto->insertar($script);
+
+            return true
+
+        }
+
+    
+    public function TestmeGustaComentario()
+
+        {
+
+            $valor = $this->meGusta(1,1);
+            $this->assertTrue(true);
+            return $valor;
+        }
+
+    /**
+     * @depends  TestmeGustaComentario
+     */
+
+    public function TestmeGustaComentarioM($a)
+    {
+        $this->assertSame(true, $a);//No falló la consulta.
+
+    }
+    }
+
+?>    
